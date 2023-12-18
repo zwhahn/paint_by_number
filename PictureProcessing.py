b42a415eaddfd8a869a1effcb12e1e1da41985d2
zwhahn <hahn@cooperperkins.com>
2023-12-18 14:19:55 -0800

    Delete matplotlib

--- PictureProcessing.py
+++ PictureProcessing.py
@@ -1,19 +1,10 @@
 import cv2 as cv
-from matplotlib import pyplot as plt
 
 # Load original image
 img = cv.imread("./pa_logo.png")
+# cv.imshow("Display window", img)  # uncomment to view original image 
 
-cv.imshow("Display window", img)
-
+# Detect image edges
 edges = cv.Canny(img, 100, 200)
-
-
-plt.subplot(121)
-plt.imshow(img)
-plt.title("Original Image")
-plt.subplot(122)
-plt.imshow(edges)
-plt.title("Edge Image")
-
-plt.show()
+cv.imshow("Display window 2", edges)
+cv.waitKey(0)
