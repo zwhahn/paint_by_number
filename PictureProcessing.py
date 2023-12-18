8bad15fe5a326276c102885feb344a6c70e2cc62
zwhahn <hahn@cooperperkins.com>
2023-12-18 14:14:50 -0800

    Display edge image next to original
    OpenCV displays the correct image but matplotlib shows different colors

--- PictureProcessing.py
+++ PictureProcessing.py
@@ -1,5 +1,19 @@
 import cv2 as cv
+from matplotlib import pyplot as plt
+
+# Load original image
 img = cv.imread("./pa_logo.png")
 
 cv.imshow("Display window", img)
-k = cv.waitKey(0)
+
+edges = cv.Canny(img, 100, 200)
+
+
+plt.subplot(121)
+plt.imshow(img)
+plt.title("Original Image")
+plt.subplot(122)
+plt.imshow(edges)
+plt.title("Edge Image")
+
+plt.show()
