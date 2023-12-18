65bb5fca38308593a1e8df296eeae133c66c0755
zwhahn <hahn@cooperperkins.com>
2023-12-18 14:32:57 -0800

    Overlay images on top of original image

--- PictureProcessing.py
+++ PictureProcessing.py
@@ -5,6 +5,13 @@ img = cv.imread("./pa_logo.png")
 # cv.imshow("Display window", img)  # uncomment to view original image 
 
 # Detect image edges
-edges = cv.Canny(img, 100, 200)
+edges = cv.Canny(img, 100, 200) 
 cv.imshow("Display window 2", edges)
-cv.waitKey(0)
+
+# Overlay edges on original image
+edges_bgr = cv.cvtColor(edges, cv.COLOR_GRAY2BGR)  # convert edges to bgr
+overlay_img = img + edges_bgr
+cv.imshow("Image with Edge Detection", overlay_img)
+
+
+cv.waitKey(0)  # keep images open until any key is pressed
