f1b1420bcf01337dc14a79999f156e8a26410540
zwhahn <hahn@cooperperkins.com>
2023-12-19 15:34:27 -0800

    Improve red mask for color_circles

--- PictureProcessing.py
+++ PictureProcessing.py
@@ -2,9 +2,9 @@ import cv2 as cv
 import numpy as np
 
 # Load original image
-# img = cv.imread("./pa_logo.png")
+img = cv.imread("./pa_logo.png")
 # img = cv.imread("./clifford.jpg")
-img = cv.imread("./color_circles.jpg")
+# img = cv.imread("./color_circles.jpg")
 cv.imshow("Original Image", img)  # uncomment to view original image 
 
 # Detect image edges
@@ -26,8 +26,8 @@ upper_hsv2 = np.array([179, 255, 255])
 # Define color dictionary (credit: Ari Hashemian https://stackoverflow.com/questions/36817133/identifying-the-range-of-a-color-in-hsv-using-opencv)
 color_dict_HSV = {'black': [[179, 255, 30], [0, 0, 0]],
               'white': [[179, 18, 255], [0, 0, 231]],
-              'red1': [[179, 255, 255], [159, 50, 20]],
-              'red2': [[9, 255, 255], [0, 50, 20]],
+              'red1': [[179, 255, 255], [179, 255, 255]],
+              'red2': [[4, 255, 255], [0, 0, 0]],
               'green': [[89, 255, 255], [36, 50, 70]],
               'blue': [[135, 255, 255], [90, 0, 0]],
               'yellow': [[35, 255, 255], [25, 50, 70]],
