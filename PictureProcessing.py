1d4f679319823627a1f563a30cc1a01b92bd00e1
zwhahn <hahn@cooperperkins.com>
2023-12-19 15:13:09 -0800

    Improve blue mask
    It is able to identify blue part of color_circles but it also includes some
    parts of the red section.

--- PictureProcessing.py
+++ PictureProcessing.py
@@ -3,8 +3,9 @@ import numpy as np
 
 # Load original image
 # img = cv.imread("./pa_logo.png")
-img = cv.imread("./clifford.jpg")
-cv.imshow("Display window", img)  # uncomment to view original image 
+# img = cv.imread("./clifford.jpg")
+img = cv.imread("./color_circles.jpg")
+cv.imshow("Original Image", img)  # uncomment to view original image 
 
 # Detect image edges
 edges = cv.Canny(img, 100, 200) 
@@ -25,10 +26,10 @@ upper_hsv2 = np.array([179, 255, 255])
 # Define color dictionary (credit: Ari Hashemian https://stackoverflow.com/questions/36817133/identifying-the-range-of-a-color-in-hsv-using-opencv)
 color_dict_HSV = {'black': [[179, 255, 30], [0, 0, 0]],
               'white': [[179, 18, 255], [0, 0, 231]],
-              'red1': [[179, 255, 255], [159, 50, 70]],
-              'red2': [[9, 255, 255], [0, 50, 70]],
+              'red1': [[179, 255, 255], [159, 50, 20]],
+              'red2': [[9, 255, 255], [0, 50, 20]],
               'green': [[89, 255, 255], [36, 50, 70]],
-              'blue': [[128, 255, 255], [90, 50, 70]],
+              'blue': [[135, 255, 255], [90, 0, 0]],
               'yellow': [[35, 255, 255], [25, 50, 70]],
               'purple': [[158, 255, 255], [129, 50, 70]],
               'orange': [[24, 255, 255], [10, 50, 70]],
@@ -36,18 +37,24 @@ color_dict_HSV = {'black': [[179, 255, 30], [0, 0, 0]],
 
 
 # Generating red mask
-red_lower_mask = cv.inRange(img, np.array(color_dict_HSV['red1'][1]), np.array(color_dict_HSV['red1'][0]))
-red_upper_mask = cv.inRange(img, np.array(color_dict_HSV['red2'][1]), np.array(color_dict_HSV['red2'][0]))
+red_lower_mask = cv.inRange(img, np.array(color_dict_HSV['red2'][1]), np.array(color_dict_HSV['red2'][0]))
+red_upper_mask = cv.inRange(img, np.array(color_dict_HSV['red1'][1]), np.array(color_dict_HSV['red1'][0]))
 full_red_mask = red_lower_mask + red_upper_mask
 
 red_detected_img = cv.bitwise_and(img, img, mask = full_red_mask)
-cv.imshow("Detected images", red_detected_img)
+cv.imshow("Red Detected Ijmages", red_detected_img)
 
 # Generate black mask
 black_mask = cv.inRange(img, np.array(color_dict_HSV["black"][1]), np.array(color_dict_HSV["black"][0]))
 black_detected_img = cv.bitwise_and(img, img, mask = black_mask)
 
 cv.imshow("Black Detected Images", black_detected_img)
+
+# Generate blue mask
+blue_mask = cv.inRange(img, np.array(color_dict_HSV["blue"][1]), np.array(color_dict_HSV["blue"][0]))
+blue_detected_img = cv.bitwise_and(img, img, mask = blue_mask)
+
+cv.imshow("Blue Detected Images", blue_detected_img)
 
 
 cv.waitKey(0)  # keep images open until any key is pressed
