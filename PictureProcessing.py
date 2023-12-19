861d25ed5bcb34abe8d42f7490be0c88a5b72a1e
zwhahn <hahn@cooperperkins.com>
2023-12-19 14:37:49 -0800

    Add color dictionary

--- PictureProcessing.py
+++ PictureProcessing.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # Load original image
 img = cv.imread("./pa_logo.png")
-# cv.imshow("Display window", img)  # uncomment to view original image 
+cv.imshow("Display window", img)  # uncomment to view original image 
 
 # Detect image edges
 edges = cv.Canny(img, 100, 200) 
@@ -12,14 +12,27 @@ edges = cv.Canny(img, 100, 200)
 # Overlay edges on original image
 edges_bgr = cv.cvtColor(edges, cv.COLOR_GRAY2BGR)  # convert edges to bgr
 overlay_img = img + edges_bgr
-cv.imshow("Image with Edge Detection", overlay_img)
+# cv.imshow("Image with Edge Detection", overlay_img)
 
 # Lower and higher HSV of red
-lower_hsv1 = np.array([0, 10, 25])
+lower_hsv1 = np.array([0, 100, 100])
 upper_hsv1 = np.array([10, 255, 255])
 
-lower_hsv2 = np.array([120, 10, 20])
+lower_hsv2 = np.array([160, 100, 100])
 upper_hsv2 = np.array([179, 255, 255])
+
+# Define color dictionary (credit: Ari Hashemian https://stackoverflow.com/questions/36817133/identifying-the-range-of-a-color-in-hsv-using-opencv)
+color_dict_HSV = {'black': [[180, 255, 30], [0, 0, 0]],
+              'white': [[180, 18, 255], [0, 0, 231]],
+              'red1': [[180, 255, 255], [159, 50, 70]],
+              'red2': [[9, 255, 255], [0, 50, 70]],
+              'green': [[89, 255, 255], [36, 50, 70]],
+              'blue': [[128, 255, 255], [90, 50, 70]],
+              'yellow': [[35, 255, 255], [25, 50, 70]],
+              'purple': [[158, 255, 255], [129, 50, 70]],
+              'orange': [[24, 255, 255], [10, 50, 70]],
+              'gray': [[180, 18, 230], [0, 0, 40]]}
+
 
 # Generating mask
 lower_mask = cv.inRange(img, lower_hsv1, upper_hsv1)
