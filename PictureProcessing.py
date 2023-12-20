a5b8060aa0f1cddeadba9ac620141d98688e29de
zwhahn <hahn@cooperperkins.com>
2023-12-20 12:42:12 -0800

    Add kmeans operation and convert back uint8
    Outputs correct image but the picture is too large to display all of it.

--- PictureProcessing.py
+++ PictureProcessing.py
@@ -1,8 +1,8 @@
 import cv2 as cv
 import numpy as np
 
-# img = cv.imread("./pa_logo.png")
-img = cv.imread("./golden_gate_bridge.jpg")
+img = cv.imread("./pa_logo.png")
+# img = cv.imread("./golden_gate_bridge.jpg")
 
 # Reshape the image to be a 2D array with 3 channels. 
 ''' The value -1 the number of rows needed is calculated 
@@ -19,6 +19,15 @@ img_reshape = np.float32(img_reshape)
 '''cv.TERM_CRITERIA_EPS indicates that the algorithm should stop when the specified accuracy (epsilon) is reached.
 cv.TERM_CRITERIA_MAX_ITER indicates that the algorithm should stop after the specified number of iterations (max_iter) 1. '''
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)  # stop criteria, epsilon, max_iter
+K = 8  # number of clusters (or colors)
+ret, label, center = cv.kmeans(img_reshape, K, None, criteria, 10, cv.KMEANS_RANDOM_CENTERS)
+
+# Convert back to uint8
+center = np.uint8(center)
+res = center[label.flatten()]
+res2 = res.reshape((img.shape))
+
+cv.imshow('res2', res2)
 
 cv.imshow("Display window", img)
 k = cv.waitKey(0)
