928f57698d752fd0ecc864f75fc51f39dfb73910
zwhahn <hahn@cooperperkins.com>
2024-05-05 11:14:39 -0700

    Print "Hello World" on pdf

--- PrinterFormat.py
+++ PrinterFormat.py
@@ -4,6 +4,8 @@ from reportlab.lib.pagesizes import LETTER
 # Create blank PDF 
 canvas = Canvas("PAintByNumber.pdf", pagesize=LETTER)
 
+canvas.drawString(50, 50,"Hello World")
+
 # Save pdf
 canvas.save()
 
