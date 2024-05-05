d613a4f229fef0c969e80172f704fbd2394a9147
zwhahn <hahn@cooperperkins.com>
2024-05-05 11:10:08 -0700

    Create blank pdf titled 'PAintByNumber.pdf'
    Size 8.5"x11"

--- PrinterFormat.py
+++ PrinterFormat.py
@@ -1,1 +1,9 @@
-import reportlab
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.pagesizes import LETTER
+
+# Create blank PDF 
+canvas = Canvas("PAintByNumber.pdf", pagesize=LETTER)
+
+# Save pdf
+canvas.save()
+
