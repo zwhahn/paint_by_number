d67bc354eeb0da53df834d8e4ce2372bfb9c9feb
zwhahn <hahn@cooperperkins.com>
2024-05-08 18:19:40 -0700

    Remove "Hello World"

--- PrinterFormat.py
+++ PrinterFormat.py
@@ -4,7 +4,6 @@ from reportlab.lib.pagesizes import LETTER
 def CreatePDF(img):
     # Create blank PDF 
     canvas = Canvas("PAintByNumber.pdf", pagesize=LETTER)
-    canvas.drawString(50, 50,"Hello World")
 
     canvas.drawImage(img, 30, 310, width=550, preserveAspectRatio=True)
 
