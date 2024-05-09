63f12af20405c3200bd2ac4df357108ff503d137
zwhahn <hahn@cooperperkins.com>
2024-05-08 18:32:10 -0700

    Add PA logo to PDF

--- PrinterFormat.py
+++ PrinterFormat.py
@@ -5,7 +5,11 @@ def CreatePDF(img):
     # Create blank PDF 
     canvas = Canvas("PAintByNumber.pdf", pagesize=LETTER)
 
+    # Add the paint-by-number image
     canvas.drawImage(img, 30, 310, width=550, preserveAspectRatio=True)
+
+    # Add PA logo 
+    canvas.drawImage("./images/pa_logo.png", 0, 0, width=150, preserveAspectRatio=True)
 
     # Save pdf to parent directory
     canvas.save()
