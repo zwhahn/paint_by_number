afd3a898da4d64cd7705b729be76dc2b1d0427a1
zwhahn <hahn@cooperperkins.com>
2024-05-08 18:12:27 -0700

    Add image display to PDF and create function CreatePDF

--- PrinterFormat.py
+++ PrinterFormat.py
@@ -1,11 +1,12 @@
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib.pagesizes import LETTER
 
-# Create blank PDF 
-canvas = Canvas("PAintByNumber.pdf", pagesize=LETTER)
+def CreatePDF(img):
+    # Create blank PDF 
+    canvas = Canvas("PAintByNumber.pdf", pagesize=LETTER)
+    canvas.drawString(50, 50,"Hello World")
 
-canvas.drawString(50, 50,"Hello World")
+    canvas.drawImage(img, 30, 310, width=550, preserveAspectRatio=True)
 
-# Save pdf
-canvas.save()
-
+    # Save pdf to parent directory
+    canvas.save()
