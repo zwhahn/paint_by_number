d45a85f32fccfc364b1a7020a51c6bc554581ec2
zwhahn <hahn@cooperperkins.com>
2024-05-14 10:58:04 -0700

    Iterate through all colors in base_colors and draw them into squares
    The RGB values need to be between 0-1 so the num_to_range function is used to map the 0-255 values to this range.

--- PrinterFormat.py
+++ PrinterFormat.py
@@ -16,8 +16,21 @@ def CreatePDF(img, base_colors):
     canvas.drawImage(pa_logo_path, 7.1*inch, 0.3*inch, preserveAspectRatio=True)
 
     # Add color/number legend
-    canvas.setFillColorRGB(255, 0, 0)
-    canvas.rect(2*inch, 2*inch, 2*inch, 2*inch, fill=True)
+    for num in range(len(base_colors)):
+        color = base_colors[num]
+        print("color:", color)
+        print("num:", num)
+        pos_x = num*inch
+        pos_y = 1*inch
+        R_map = num_to_range(color[2])
+        G_map = num_to_range(color[1])
+        B_map = num_to_range(color[0])
+        canvas.setFillColorRGB(R_map, G_map, B_map)
+        canvas.rect(pos_x, pos_y, 0.5*inch, 0.5*inch, fill=True)
     
     # Save pdf to parent directory
     canvas.save()
+
+# Source: https://www.30secondsofcode.org/python/s/num-to-range/
+def num_to_range(num, inMin=0, inMax=255, outMin=0, outMax=1):
+  return outMin + (float(num - inMin) / float(inMax - inMin) * (outMax- outMin))
