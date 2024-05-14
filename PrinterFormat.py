f8822df855af2ae7fd8b4b0269d8ca99184508bc
zwhahn <hahn@cooperperkins.com>
2024-05-14 10:43:34 -0700

    Draw simple rectangle as test

--- PrinterFormat.py
+++ PrinterFormat.py
@@ -1,10 +1,11 @@
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib.pagesizes import LETTER
 from reportlab.lib.units import inch
+from reportlab.graphics import shapes
 
 pa_logo_path = "./images/pa_logo.png"
 
-def CreatePDF(img):
+def CreatePDF(img, base_colors):
     # Create blank PDF 
     canvas = Canvas("PAintByNumber.pdf", pagesize=LETTER)
 
@@ -14,5 +15,9 @@ def CreatePDF(img):
     # Add PA logo 
     canvas.drawImage(pa_logo_path, 7.1*inch, 0.3*inch, preserveAspectRatio=True)
 
+    # Add color/number legend
+    canvas.setFillColorRGB(255, 0, 0)
+    canvas.rect(2*inch, 2*inch, 2*inch, 2*inch, fill=True)
+    
     # Save pdf to parent directory
     canvas.save()
