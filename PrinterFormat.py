c50a41bf20be25139236263bd4446ee14c0ec480
zwhahn <hahn@cooperperkins.com>
2024-05-14 16:08:36 -0700

    Print squares with RGB values from the base_colors library

--- PrinterFormat.py
+++ PrinterFormat.py
@@ -4,7 +4,7 @@ from reportlab.lib.units import inch
 from reportlab.graphics import shapes
 
 pa_logo_path = "./images/pa_logo.png"
-color_square_size = 0.5*inch
+color_square_size = 0.5*inch # [inch]
 
 def CreatePDF(img, base_colors):
     # Create blank PDF 
@@ -19,8 +19,44 @@ def CreatePDF(img, base_colors):
     # Add color/number legend
     for num in range(len(base_colors)):
         color = base_colors[num]
-        pos_x = num*inch
-        pos_y = 1*inch
+
+        # Desired center position
+        if 0 <= num <= 2:
+            center_y = 3*inch
+            if num == 0:
+                center_x = (8.5/3)*inch
+            if num == 1:   
+                center_x = (8.5/2)*inch
+            if num == 2:
+                center_x = ((2*8.5)/3)*inch
+
+        if 3 <= num <= 5:
+            center_y = 2*inch
+            if num == 3:
+                center_x = (8.5/3)*inch
+            if num == 4:   
+                center_x = (8.5/2)*inch
+            if num == 5:
+                center_x = ((2*8.5)/3)*inch
+
+        if 6 <= num <= 8:
+            center_y = 1*inch
+            if num == 6:
+                center_x = (8.5/3)*inch
+            if num == 7:   
+                center_x = (8.5/2)*inch
+            if num == 8:
+                center_x = ((2*8.5)/3)*inch
+
+        print("center_x:", center_x)
+        print("center_y:", center_y)
+
+        # Rectangle position is placed by bottom left corner, so that position is calculated
+        pos_x = center_x - (color_square_size/2)
+        pos_y = center_y - (color_square_size/2)
+
+        print("pos_x:", pos_x)
+        print("pos_y:", pos_y)
 
         # Map 0-255 value to 0-1 range used by reportlab
         # The base colors are in BGR format so the color orders are reversed
