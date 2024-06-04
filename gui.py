4dd1161c130af662f75c95fe81abe196f7e2b900
zwhahn <hahn@cooperperkins.com>
2024-06-04 14:53:43 -0700

    Countdown displayed on top screen

--- gui.py
+++ gui.py
@@ -9,6 +9,8 @@ class WebcamApp:
         self.window.title("Webcam App")
 
         self.current_image = None
+        self.remaining_time = None
+        self.start_time = None
         self.vid = cv.VideoCapture(0)
         self.canvas = tk.Canvas(window, width = 640, height=480)
         self.canvas.pack()
@@ -21,21 +23,29 @@ class WebcamApp:
     def update_webcam(self):
         ret, self.frame = self.vid.read()
 
-        if ret:
-            self.current_image = Image.fromarray(cv.cvtColor(self.frame, cv.COLOR_BGR2RGB))
-            self.photo = ImageTk.PhotoImage(image=self.current_image)
+        if self.start_time is not None:  # Dont add text to image until timer has started
+            self.remaining_time = int(3 - ((time.time() - self.start_time) //1))
+            if self.remaining_time > 0:  # If there is no remaining time than we should not alter the frame, otherwise add countdown
+                cv.putText(self.frame, str(self.remaining_time), (200, 300), cv.FONT_HERSHEY_SIMPLEX, 7, (0, 0, 0), 20, cv.FILLED) 
+      
+        # Display video feed
+        self.current_image = Image.fromarray(cv.cvtColor(self.frame, cv.COLOR_BGR2RGB))
+        self.photo = ImageTk.PhotoImage(image=self.current_image)
 
-            self.canvas.create_image(0,0,image=self.photo, anchor=tk.NW)
+        self.canvas.create_image(0,0,image=self.photo, anchor=tk.NW)
 
 
-            self.window.after(10, self.update_webcam)
+        self.window.after(10, self.update_webcam)
 
+        # Once the countdown is over, the image is captured and saved to the images folder
+        if self.remaining_time == 0:
+            print("Image Captured!")
+            cv.imwrite('./images/capture.png', self.frame)  # overwrites the last captured image
+    
+    
     def capture_image(self):
-        print(self.current_image)
-        print("Trying to take picture")
-        if self.current_image is not None:
-            print("Picture taken!")
-            cv.imwrite('./images/capture.png', self.frame)
+        self.start_time = time.time()
+
          
 
 root = tk.Tk()
