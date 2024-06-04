81b5591c0c947aef5964a3e309d902d11ac1f513
zwhahn <hahn@cooperperkins.com>
2024-06-04 11:23:15 -0700

    Create app that displays camera feed

---
+++ gui.py
@@ -0,0 +1,32 @@
+from tkinter import *
+import cv2 as cv
+from PIL import Image, ImageTk
+
+vid = cv.VideoCapture(0)
+
+app = Tk()
+app.bind('<Escape>', lambda e: app.quit())
+
+label_widget = Label(app)
+label_widget.pack()
+
+
+def open_camera():
+    _, frame = vid.read()
+
+    opencv_image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+    captured_image = Image.fromarray(opencv_image)
+
+    photo_image = ImageTk.PhotoImage(image=captured_image)
+
+    label_widget.photo_image = photo_image
+
+    label_widget.configure(image = photo_image)
+
+    label_widget.after(10, open_camera)
+
+button1 = Button(app, text="Open Camera", command=open_camera)
+button1.pack()
+
+app.mainloop()
