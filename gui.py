ae759dc46be994fb4086e5b712ebe136c39eef9d
zwhahn <hahn@cooperperkins.com>
2024-06-04 14:31:31 -0700

    Create class WebcamApp
    Stream webcam video

--- gui.py
+++ gui.py
@@ -1,31 +1,31 @@
-from tkinter import *
+import tkinter as tk
 import cv2 as cv
 from PIL import Image, ImageTk
+import time
 
-vid = cv.VideoCapture(0)
-vid.set(cv.CAP_PROP_FRAME_WIDTH, 3000)
+class WebcamApp:
+    def __init__(self, window):
+        self.window = window
+        self.window.title("Webcam App")
 
-app = Tk()
-app.attributes('-fullscreen',True)
-app.bind('<Escape>', lambda e: app.quit())  # close app with esc
+        self.vid = cv.VideoCapture(0)
+        self.canvas = tk.Canvas(window, width = 640, height=480)
+        self.canvas.pack()
+        self.update_webcam()
 
-label_widget = Label(app)
-label_widget.pack()
+    def update_webcam(self):
+        ret, frame = self.vid.read()
+
+        if ret:
+            self.current_image = Image.fromarray(cv.cvtColor(frame, cv.COLOR_BGR2RGB))
+            self.photo = ImageTk.PhotoImage(image=self.current_image)
+
+            self.canvas.create_image(0,0,image=self.photo, anchor=tk.NW)
 
 
-def open_camera():
-    _, frame = vid.read()
+            self.window.after(10, self.update_webcam)
+         
 
-    opencv_image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-    captured_image = Image.fromarray(opencv_image)
-    photo_image = ImageTk.PhotoImage(image=captured_image)
-
-    label_widget.photo_image = photo_image
-    label_widget.configure(image = photo_image)
-
-    label_widget.after(10, open_camera)
-
-button1 = Button(app, text="Open Camera", command=open_camera)
-button1.pack()
-
-app.mainloop()
+root = tk.Tk()
+app = WebcamApp(root)
+root.mainloop()
